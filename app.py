be36d4120a6c4625697c271601362fdbdbd54a36
ddake25 <dake@cua.edu>
2025-06-01 04:24:20 -0400

    Successfully created and runned 'My first Graph'

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
-print("Hello World")
+# Import packages
+from typing import TypedDict
+from langgraph.graph import StateGraph, START, END
+
+
+# Create Agent State
+class AgentState(TypedDict):
+    name: str
+    messages: list[dict]
+    
+# Create node to onboard user
+def onboarding_node(state: AgentState):
+    user_name = input("What is your name: ")
+    state["name"] = user_name
+    state["messages"] = f"Hey {state['name']} 🙂\n We are excited to have you join our department!"
+    
+    return state
+
+# Create Graph
+graph_builder = StateGraph(AgentState)
+
+graph_builder.add_node("Onboarder", onboarding_node)
+graph_builder.add_edge(START, "Onboarder")
+graph_builder.add_edge("Onboarder", END)
+
+graph = graph_builder.compile()
